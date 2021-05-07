773ba3057bab40a3e124c20c9bac5682caf68031
Joe Small <small.joseph@outlook.com>
2021-05-07 19:38:57 -0400

    new commit

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//GET ALL COMMENTS
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//POST A COMMENT
+router.post('/', withAuth, (req, res) => {
+    // {
+    //     comment_text: "This is the comment",
+    //     user_id: 1,
+    //     post_id: 2
+    // }
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+//DELETE A COMMENT
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'Nope, try again' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
